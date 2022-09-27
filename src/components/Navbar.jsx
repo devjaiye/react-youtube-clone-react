09d3a0b48d9dd6d7eb113526e4e6d35941871df1
devjaiye <dayojaiye74@gmail.com>
2022-09-27 08:59:18 +0100

    Youtube Clone

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import {Stack} from '@mui/material' //..importing Stack component from MUI for the Navbar row 
+import {Link} from 'react-router-dom'
+
+const Navbar = () => {
+    return (
+      <div>NavBar</div>
+    )
+  }
+  
+  export default Navbar
---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+//...exporting all the components here before importing it in App.js
+
+export {default as Navbar} from './Navbar'
+export {default as Feed} from './Feed'
+export {default as VideoDetail} from './VideoDetail'
+export {default as SearchFeed} from './SearchFeed'
+export {default as ChannelDetail} from './ChannelDetail'
+
